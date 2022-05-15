230357583ba044955f15b62c678fa41659a9ad6e
aparna21A91A6137 <97721064+aparna21A91A6137@users.noreply.github.com>
2022-05-15 12:03:07 +0530

    n=int(input()) arr=list(map(int,input().split())) k=int(input()) max=0 for i in range(len(arr)):     if arr[i]+k>=max:         max=arr[i]         print("True",end=' ')     else:         print("False",end=' ')

---
+++ Greatest_Number_of_Candies.py
@@ -0,0 +1,10 @@
+n=int(input())
+arr=list(map(int,input().split()))
+k=int(input())
+max=0
+for i in range(len(arr)):
+    if arr[i]+k>=max:
+        max=arr[i]
+        print("True",end=' ')
+    else:
+        print("False",end=' ')
