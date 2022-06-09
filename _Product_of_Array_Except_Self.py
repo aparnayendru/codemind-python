84824baa19447b885a3a7168e4fcf2606a2e2353
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-06-09 12:12:25 +0530

    n=int(input()) prod=1 arr=list(map(int,input().split())) for i in range(len(arr)):     prod=1     for j in range(len(arr)):         if i==j:             continue         else:             prod=prod*arr[j]     print(prod,end=" ")

---
+++ _Product_of_Array_Except_Self.py
@@ -0,0 +1,11 @@
+n=int(input())
+prod=1
+arr=list(map(int,input().split()))
+for i in range(len(arr)):
+    prod=1
+    for j in range(len(arr)):
+        if i==j:
+            continue
+        else:
+            prod=prod*arr[j]
+    print(prod,end=" ")
