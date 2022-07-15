b8bf5451dfa56fd9b61f130651d5c2e0650ace28
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-07-15 13:21:35 +0530

    s=input() x=s.split() for words in x:     a=min(words)     b=max(words)     diff=abs(ord(a)-ord(b))     print(diff,end=" ")

--- absolute_difference_of_small_and_large.py
+++ absolute_difference_of_small_and_large.py
@@ -1,12 +1,7 @@
 s=input()
-s=s.split()
-a=0
-b=0
-diff=0
-for words in s:
-    c=min(words)
-    d=max(words)
-    a=ord(c)
-    b=ord(d)
-    diff=abs(a-b)
+x=s.split()
+for words in x:
+    a=min(words)
+    b=max(words)
+    diff=abs(ord(a)-ord(b))
     print(diff,end=" ")
