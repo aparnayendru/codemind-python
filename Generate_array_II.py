71b6f7f0c9f9fef6e2b22d969246fe7b50ac8657
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-11-27 19:11:27 +0530

    n=int(input()) arr=list(map(int,input().split())) for i in range(0,n,2):     k=arr[i+1]     count=1     while count<=k:         print(arr[i],end=" ")         count+=1

--- Generate_array_II.py
+++ Generate_array_II.py
@@ -1,7 +1,8 @@
 n=int(input())
 arr=list(map(int,input().split()))
-countt=0
-for i in range(0,n-1,2):
-    k=arr[i]
-    m=arr[i+1]
-    print((str(k)+" ") * m,end="")
+for i in range(0,n,2):
+    k=arr[i+1]
+    count=1
+    while count<=k:
+        print(arr[i],end=" ")
+        count+=1
