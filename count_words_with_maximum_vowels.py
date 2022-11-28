367fa2eef65c02bcb1a770d5d94ada08a53c6562
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-11-28 19:42:03 +0530

    s=input().lower().split() x='aeiou' count=0 maxx=0 maxi=0 for words in s:     count=0     for i in words:         if i in x:             count+=1     if count>maxx:         maxx=count for words in s:     count=0     for i in words:         if i in x:             count+=1     if count==maxx:         maxi+=1 print(maxi)

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,20 @@
+s=input().lower().split()
+x='aeiou'
+count=0
+maxx=0
+maxi=0
+for words in s:
+    count=0
+    for i in words:
+        if i in x:
+            count+=1
+    if count>maxx:
+        maxx=count
+for words in s:
+    count=0
+    for i in words:
+        if i in x:
+            count+=1
+    if count==maxx:
+        maxi+=1
+print(maxi)
