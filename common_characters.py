2e345e9f9e5b4e36bca7c6d4498d6516e5d7bb60
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-11-30 19:36:00 +0530

    s=input().lower() s=s.replace(" ","") #print(s) t=input().lower() t=t.replace(" ","") a=[] x=0 for ch in set(s):     if ch in set(t):         a.append(ch)         x=1 if x==0:     print("-1") else:     a.sort()     for i in a:         print(i,end="")

--- common_characters.py
+++ common_characters.py
@@ -5,26 +5,13 @@ t=input().lower()
 t=t.replace(" ","")
 a=[]
 x=0
-#print(t)
-if len(s)>len(t):
-    for ch in set(s):
-        if ch in set(t):
-           a.append(ch)
-           x=1
-    if x==0:
-        print("-1")
-    else:
-        a.sort()
-        for i in a:
-            print(i,end="")
+for ch in set(s):
+    if ch in set(t):
+        a.append(ch)
+        x=1
+if x==0:
+    print("-1")
 else:
-    for ch in set(t):
-        if ch in set(s):
-            a.append(ch)
-            x=1
-    if x==0:
-        print("-1")
-    else:
-        a.sort()
-        for i in a:
-            print(i,end="")
+    a.sort()
+    for i in a:
+        print(i,end="")
