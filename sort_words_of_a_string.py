7cf0621512a64e3e6242d732da2ef3f480b156ca
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-11-30 16:20:50 +0530

    s=input().split() for words in s:     c=[]     d=[]     ind=[]     for i in range(len(words)):         if words[i].isalpha():             c.append(words[i])         else:             d.append(words[i])             ind.append(i)     c.sort()     for k in range(len(d)):         c.insert(ind[k],d[k])     for j in c:         print(j,end="")     print(" ",end="")

---
+++ sort_words_of_a_string.py
@@ -0,0 +1,17 @@
+s=input().split()
+for words in s:
+    c=[]
+    d=[]
+    ind=[]
+    for i in range(len(words)):
+        if words[i].isalpha():
+            c.append(words[i])
+        else:
+            d.append(words[i])
+            ind.append(i)
+    c.sort()
+    for k in range(len(d)):
+        c.insert(ind[k],d[k])
+    for j in c:
+        print(j,end="")
+    print(" ",end="")
