fb135dc82dc30744d73e3a9a2021c635e9f16db5
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-11-30 16:18:27 +0530

    s=input().split() v='aeiouAEIOU' for words in s:     c=[]     d=[]     ind=[]     for i in range(len(words)):         if words[i] not in v:             c.append(words[i])         else:             d.append(words[i])             ind.append(i)     c.sort()     for k in range(len(d)):         c.insert(ind[k],d[k])     for j in c:         print(j,end="")     print(" ",end="")

---
+++ sort_consonents.py
@@ -0,0 +1,18 @@
+s=input().split()
+v='aeiouAEIOU'
+for words in s:
+    c=[]
+    d=[]
+    ind=[]
+    for i in range(len(words)):
+        if words[i] not in v:
+            c.append(words[i])
+        else:
+            d.append(words[i])
+            ind.append(i)
+    c.sort()
+    for k in range(len(d)):
+        c.insert(ind[k],d[k])
+    for j in c:
+        print(j,end="")
+    print(" ",end="")
