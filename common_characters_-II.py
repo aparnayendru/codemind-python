75c4f9c241f51eb5db212a032cc454f3f06161ab
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-11-30 19:39:31 +0530

    s=input().lower() s=s.replace(" ","") #print(s) t=input().lower() t=t.replace(" ","") a=[] x=0 #print(t) if len(s)>len(t):     for ch in set(s):         if ch in set(t):            a.append(ch)     else:         print(len(a)) else:     for ch in set(t):         if ch in set(s):             a.append(ch)
        else:
            print(len(a))

--- common_characters_-II.py
+++ common_characters_-II.py
@@ -10,18 +10,12 @@ if len(s)>len(t):
     for ch in set(s):
         if ch in set(t):
            a.append(ch)
-           x=1
-    #if x==0:
-        #print("-1")
     else:
         print(len(a))
-    
 else:
     for ch in set(t):
         if ch in set(s):
             a.append(ch)
-            x=1
-    #if x==0:
-        #print("-1")
+            
     else:
         print(len(a))
